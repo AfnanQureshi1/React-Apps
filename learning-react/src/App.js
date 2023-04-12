9548e8780b6a2f45139ff6a479c6f78f8471d7c2
Afnan Qureshi <Afnan.qureshi.x1@gmail.com>
2023-04-12 23:09:37 +0500

    react app initiated

---
+++ learning-react/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <>
+    {/*shortcut emmets*/}
+      <div className="blank"></div>{/*div.blank*/}
+      <li className="name">hi</li>{/*#li.name*/}
+      <ul className="list"></ul> {/*il.list*/}
+      <div className="content"></div> {/*.content*/}
+      <div id="content-id"></div> {/*#content-id*/}
+    </>
+    
+  );
+}
+
+export default App;
